278fc0bdd51e173c692a1080dee805a3f14b67cf
randyS30 <randy_saravia@usmp.pe>
2025-09-15 17:28:35 -0500

    railway

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,11 +5,10 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: process.env.PGUSER || "postgres",
-  host: process.env.PGHOST || "localhost",
-  database: process.env.PGDATABASE || "tesisdb",
-  password: process.env.PGPASSWORD || "1234",
-  port: process.env.PGPORT ? parseInt(process.env.PGPORT) : 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Railway requiere SSL
+  },
 });
 
 export default pool;
